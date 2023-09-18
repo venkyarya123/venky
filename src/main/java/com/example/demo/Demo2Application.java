d599523566af55c2a72272c59132e0b0440c08db
unknown <venkateshvajrapu592001@gmail.com>
2023-09-18 14:18:50 +0530

    first commiyt

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo2Application {
+
+	@GetMapping
+	public String helloworld()
+	{
+		return "Hello world";
+	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+}
